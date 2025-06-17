6cca0074333ba410ddb14ad762768edec17f0858
aieghbal <aieghbal.me@gmail.com>
2025-06-17 11:59:40 +0330

    Add vuetify3

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,23 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './components/App.vue';
 
-createApp(App).mount('#app');
+// Vuetify imports
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Fonts
+import '@mdi/font/css/materialdesignicons.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+// Create Vuetify instance
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App)
+    .use(vuetify)
+    .mount('#app');
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,13 @@
 <template>
-    <h1>Hello from Vue 3!</h1>
+    <v-app>
+        <v-main>
+            <v-container class="text-center">
+                <v-btn color="primary" class="ma-2">دکمه تست Vuetify</v-btn>
+                <h1>کار می‌کنه 🎉</h1>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup>
 </script>
-
-<style scoped>
-h1 {
-    color: #42b983;
-}
-</style>
