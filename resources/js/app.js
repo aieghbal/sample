82707fdb2076d24ddba54045aef47ef4ff963cdb
aieghbal <aieghbal.me@gmail.com>
2025-06-21 09:54:05 +0330

    install breeze for auth system

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,27 @@
+import '../css/app.css';
 import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue';
 
-// Vuetify imports
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-// Fonts
-import '@mdi/font/css/materialdesignicons.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-// Create Vuetify instance
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
-createApp(App)
-    .use(vuetify)
-    .mount('#app');
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<div id="app"></div>
-</body>
-</html>
